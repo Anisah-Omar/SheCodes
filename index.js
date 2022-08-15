109344b669805f8e50d395fb6f795c08bca6899e
Anisah Omar <71156452+Anisah-Omar@users.noreply.github.com>
2022-08-15 23:21:19 +0300

    api key

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function displayWeatherCondition(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "628cb8a28ddea2a7ac4d0391b0682732";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
