ccdf90ab0d7f0f032cd48f1975636fa77595fedc
Anisah Omar <71156452+Anisah-Omar@users.noreply.github.com>
2022-08-15 23:19:39 +0300

    api key

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "628cb8a28ddea2a7ac4d0391b0682732";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
     position.coords.latitude
   }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
